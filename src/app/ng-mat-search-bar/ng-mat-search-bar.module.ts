b637c059c093730e5d7731797c4e79e2668fc5f3
Tom Müller <kontakt@tommueller.me>
2018-02-06 16:33:05 +0100

    feat: add ripple effect

--- src/app/ng-mat-search-bar/ng-mat-search-bar.module.ts
+++ src/app/ng-mat-search-bar/ng-mat-search-bar.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatIconModule, MatInputModule } from '@angular/material';
+import { MatIconModule, MatInputModule, MatRippleModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSearchBarComponent } from './mat-search-bar/mat-search-bar.component';
@@ -10,7 +10,8 @@ import { MatSearchBarComponent } from './mat-search-bar/mat-search-bar.component
     BrowserAnimationsModule,
     CommonModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatRippleModule
   ],
   exports: [
     MatSearchBarComponent
